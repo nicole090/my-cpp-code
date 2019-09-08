c84f7b1ee54c2148ba6d96a5b4189b8eb38e3eeb
童 <tong@tongdeMacBook-Pro.local>
2019-09-08 20:05:25 +0800

    2

---
+++ 求平均值.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    double a;
+    double b;
+    double c;
+    double d;
+    double ave=(a+b+c+d)/4;
+    scanf("%lf %lf %lf %lf",&a,&b,&c,&d);
+    printf("%.2lf",ave=(a+b+c+d)/4);
+return 0;
+}
