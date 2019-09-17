1a0d14d41250e8f407302227d4aa51184f1709cd
童 <tong@tongdeMacBook-Pro.local>
2019-09-17 14:10:13 +0800

    7

---
+++ calculate.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d divided by %d equals %d\n", 15, 4, 15 / 4);
+    printf("%d precent %d eqauls %d\n", 15, 4, 15 % 4);
+    printf("%d %d %d %d %d\n", 5, 3, 6, 4, 5 * 3 - 6 * 4);
+    return 0;
+}
