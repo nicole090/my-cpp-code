1d7f7b130a22b44942443a4115466b0666b565ef
童 <tong@tongdeMacBook-Pro.local>
2019-09-17 16:26:03 +0800

    7.6

---
+++ caculaate2.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("%f times %f equals %f\n", 3.0, 5.0, 3.0 * 5.0);
+    printf("%f times %f minus %f equals %f\n", 7.1, 8.3, 2.2, 7.1 * 8.3 - 2.2);
+    printf("%f * %f / %f = %f\n",6.1, 5.0, 3.2, 6.1 * 5.0 / 3.2);
+    return 0;
+}
